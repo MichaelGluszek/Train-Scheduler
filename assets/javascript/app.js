6e4c15f0b6b1505393eff19e77520f10d0c6f771
Mike Gluszek <mgluszek710@gmail.com>
2019-05-13 19:57:58 -0500

    initiate!

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+$(function(){
+
+let firebaseConfig = {
+    apiKey: "AIzaSyBi0-kaQibSOPKrIipQ5GTWaPIJgHKoDp0",
+    authDomain: "trainscheduler-31cb9.firebaseapp.com",
+    databaseURL: "https://trainscheduler-31cb9.firebaseio.com",
+    projectId: "trainscheduler-31cb9",
+    storageBucket: "trainscheduler-31cb9.appspot.com",
+    messagingSenderId: "101421284499",
+    appId: "1:101421284499:web:df9058a8e8d5fc53"
+}
+
+
+firebase.initializeApp(firebaseConfig);
+
+
+let database = firebase.database();
+  };
